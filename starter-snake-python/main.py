3476785110c9f976aa384514b4aacd0558177bbf
snazziington <amini4life@gmail.com>
2026-03-25 13:01:16 +0100

    updoot

--- starter-snake-python/main.py
+++ starter-snake-python/main.py
@@ -22,10 +22,10 @@ def info() -> typing.Dict:
 
     return {
         "apiversion": "1",
-        "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Choose color
-        "head": "default",  # TODO: Choose head
-        "tail": "default",  # TODO: Choose tail
+        "author": "Snazziington",  # TODO: Your Battlesnake Username
+        "color": "#F09148",  # TODO: Choose color
+        "head": "beluga",  # TODO: Choose head
+        "tail": "do-sammy",  # TODO: Choose tail
     }
 
 
