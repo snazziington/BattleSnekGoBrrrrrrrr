011dd78b25154ce788b115a93ecf02ab78ff4206
snazziington <amini4life@gmail.com>
2026-03-31 11:28:09 +0200

    (my code) snakes are aware of each other and try(?) to avoid each other
    also started writing code where the snake avoids moving into the potential moves of other snakes. didn't manage to finish it though

--- starter-snake-python/zcomboCode.py
+++ starter-snake-python/zcomboCode.py
@@ -12,7 +12,7 @@ def info() -> typing.Dict:
     return {
        "apiversion":"1",
        "author":"Snazziington",  # TODO: Your Battlesnake Username
-       "color":"#E0B057",  # TODO: Choose color
+       "color":"#6B57E0",  # TODO: Choose color
        "head":"beluga",  # TODO: Choose head
        "tail":"do-sammy",  # TODO: Choose tail
     }
@@ -27,30 +27,20 @@ def end(game_state: typing.Dict):
 
 # endregion
 
-# move is called on every turn and returns your next move
-# Valid moves are"up","down","left", or"right"
+# For each possible move:
+    # [0] = x vector, [1] = y vector, [2] = string direction
+moves = [[0, 1, "up"], [0, -1, "down"], [-1, 0, "left"], [1, 0, "right"]]
 
-collisionScore = -100
-# region Helpers
-def get_head(state):
-    return state["you"]["body"][0]
+collisionScore = -1000
 
-def move_point(point, move):
-    if move == "up":
-        return {"x": point["x"], "y": point["y"] + 1}
-    if move == "down":
-        return {"x": point["x"], "y": point["y"] - 1}
-    if move == "left":
-        return {"x": point["x"] - 1, "y": point["y"]}
-    if move == "right":
-        return {"x": point["x"] + 1, "y": point["y"]}
-# endregion
+floodFillWeight = 1
+chanceCollisionWeight = .75
+tailFollowWeight = .8
 
-def wallCollision(game_state: typing.Dict, nextMove, boardWidth, boardHeight):
+def wallCollision(nextMove, boardWidth, boardHeight):
     if nextMove['x'] == boardWidth or nextMove['x'] == -1 or nextMove['y'] == boardHeight or nextMove['y'] == -1:
         return True
-
-# Check Snake Occupied Tiles    
+    
 def checkSnakeTiles(game_state: typing.Dict):
     snakeBodyTiles = list()
 
@@ -60,9 +50,43 @@ def checkSnakeTiles(game_state: typing.Dict):
             snakeBodyTiles.append(xy)
     return snakeBodyTiles
 
-# Simulate The Game Move
-def apply_move(state, move):
-    new_state = {
+def floodFill(start, board, occupied): # floodEvalPosition, boardState, occupiedTiles
+    # Flood fill score for moving into wall is 0
+    if start["x"] in {0, board["width"]} or start["y"] in {0, board["height"]}:
+        return 0
+
+    else:
+        stack = [(start["x"], start["y"])]
+        visited = set()
+        count = 0
+
+        while stack:
+            x, y = stack.pop()
+            if (x, y) in visited:
+                continue
+
+            visited.add((x, y))
+            count += 1
+
+            for nx, ny in [(x + 1, y),(x - 1, y),(x, y + 1),(x, y - 1)]:
+                if (0 <= nx < board["width"] and
+                    0 <= ny < board["height"] and
+                    (nx, ny) not in occupied):
+                    stack.append((nx, ny))
+        return count
+
+def movePoint(point, move):
+    if move == "up":
+        return {"x": point["x"], "y": point["y"] + 1}
+    if move == "down":
+        return {"x": point["x"], "y": point["y"] - 1}
+    if move == "left":
+        return {"x": point["x"] - 1, "y": point["y"]}
+    if move == "right":
+        return {"x": point["x"] + 1, "y": point["y"]}
+    
+def simulateMove(state, move):
+    newState = {
         "you": {
             "body": list(state["you"]["body"]),
             "health": state["you"]["health"] - 1
@@ -70,52 +94,70 @@ def apply_move(state, move):
         "board": state["board"]
     }
 
-    head = get_head(state)
-    new_head = move_point(head, move)
+    head = state["you"]["body"][0] # TODO: use def getHead(state)
+    newHead = movePoint(head, move)
 
-    new_body = [new_head] + new_state["you"]["body"][:-1]
+    newBody = [newHead] + newState["you"]["body"][:-1]
 
     # food
     for food in state["board"]["food"]:
-        if food == new_head:
-            new_body.append(new_body[-1])
-            new_state["you"]["health"] = 100
+        if food == newHead:
+            newBody.append(newBody[-1])
+            newState["you"]["health"] = 100
 
-    new_state["you"]["body"] = new_body
-    return new_state
+    newState["you"]["body"] = newBody
+    return newState
 
-# Flood Fill Algo
-def flood_fill(start, board, occupied): # startingCoords, boardState, occupiedTiles
-    stack = [(start["x"], start["y"])]
-    visited = set()
-    count = 0
+def occupiedPositions(state):
+    occ = list()
+    # Adds current state of all snakes to the occ array
+    for s in state["board"]["snakes"]:
+        for b in s["body"]:
+            bodyCoords = {'x': 0, 'y': 0}
+            bodyCoords["x"] = b["x"]
+            bodyCoords["y"] = b["y"]
+            occ.append(bodyCoords)
+        
+    # Adds potential moves of other snakes to potPositions array
+    
+    for snake in state["board"]["snakes"]:
+        if len(snake["body"]) > len(state["you"]): # TODO: Add a "+ 1" on the right side so our snake only
+                                                   # ignores collisions with a snake *two* points smaller?
+            head = snake["body"][0]
+            for m in range (0, 4):
+                coords = (head["x"] + moves[m][0], head["y"] + moves[m][1])
+                occ.append(coords)
+    print("occc:", occ)
+    return occ
 
-    while stack:
-        x, y = stack.pop()
-        if (x, y) in visited:
-            continue
+    # TODO: Exclude our snake from this^^ code block; our snake should not avoid its own potential moves lol
 
-        visited.add((x, y))
-        count += 1
-
-        for nx, ny in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
-            if (0 <= nx < board["width"] and
-                0 <= ny < board["height"] and
-                (nx, ny) not in occupied):
-                stack.append((nx, ny))
-    return count
+def potentialCollisions(state):
+    pot = list()
+    print("youuuu", state["you"])
+    
+    for snake in state["board"]["snakes"]:
+        print("themmmm", snake["length"])
+        if snake["length"] >= state["you"]["length"]:
+            # TODO: Add a "+ 1" on the right side so our snake only ignores collisions with a
+            # snake *two* points smaller?
+            head = snake["body"][0]
+            for m in range (0, 4):
+                coords = {'x': 0, 'y': 0}
+                coords["x"] = (head["x"] + moves[m][0])
+                coords["y"] = (head["y"] + moves[m][1])
+                pot.append(coords)
+    return pot
 
 def move(game_state: typing.Dict) -> typing.Dict:
+    print("============")
     print("MOVE:", {game_state['turn']})
-
     print("Check Snake Tiles:", checkSnakeTiles(game_state))
 
     # region Initialisation
     # Snake Head + Neck
-    myHead  = game_state["you"]["body"][0]
-    tailIndex = len(game_state["you"]["body"]) - 1
-
-    myTail = game_state["you"]["body"][tailIndex]
+    myHead = game_state["you"]["body"][0]
+    myTail = game_state["you"]["body"][len(game_state["you"]["body"]) - 1]
 
     # Board dimensions
     boardWidth = game_state['board']['width']
@@ -123,39 +165,49 @@ def move(game_state: typing.Dict) -> typing.Dict:
 
     # Scores of each direction initialise as 0
     score = [0, 0, 0, 0] # up, down, left, right
-    # For each possible move:
-    # [0] = x vector, [1] = y vector, [2] = string direction
-    moves = [[0, 1,"up"], [0, -1,"down"], [-1, 0,"left"], [1, 0,"right"]]
     
     # endregion
 
     # Check Collisions
-    for n in range (0, 4): # For each possible move (up, down, left, right)
+    for m in range (0, 4): # For each possible move (up, down, left, right)
+        print("...")
         nextMove = {'x': 0, 'y': 0}
-        nextMove['x'] = myHead["x"] + moves[n][0]
-        nextMove['y'] = myHead["y"] + moves[n][1]
-
-        print(flood_fill(start, board, occupied))
+        nextMove['x'] = myHead["x"] + moves[m][0]
+        nextMove['y'] = myHead["y"] + moves[m][1]
 
         # New coords of head if doing this move
-        print("Compare:", nextMove, myTail)
-        print("If moving", moves[n][2],", head will be at:", nextMove)
+        print("If moving", moves[m][2], ", head will be at:", nextMove)
 
-        # Snake can chase own tail if safe
-        if nextMove == myTail and game_state['turn'] > 2 and game_state["you"]["health"] != 100:
-                return
-        # Avoid Wall Collisions
-        elif wallCollision(game_state, nextMove, boardWidth, boardHeight):
-                score[n] += collisionScore
-                print("Cannot move", moves[n][2],", will collide with wall")
+        # Simulates next move
+        print("Moooves: ", moves[m][2])
+        simGameState = simulateMove(game_state, moves[m][2])
+        occupiedTiles = occupiedPositions(simGameState)
+        floodFillScore = floodFillWeight * floodFill(simGameState["you"]["body"][0], simGameState["board"], occupiedTiles)
+        score[m] += floodFillScore
+
+        print("floodFillScore:", floodFillScore)
+        print("Next move:", nextMove)
+        print("Potential Collisions:", potentialCollisions(game_state))
+
+        if wallCollision(nextMove, boardWidth, boardHeight):
+                score[m] += collisionScore
+                print("Cannot move", moves[m][2], ", will collide with wall")
 
         # Avoid Snake Collisions
         elif nextMove in checkSnakeTiles(game_state):
-                    print("Check Snake Tiles:", checkSnakeTiles(game_state))
-                    score[n] += collisionScore
-                    print("Cannot move", moves[n][2],", will collide with a body")
+            if nextMove == myTail:
+                print("Chasing own tail")
+                score[m] *= tailFollowWeight
+            else:
+                print("Check Snake Tiles:", checkSnakeTiles(game_state))
+                score[m] += collisionScore
+                print("Cannot move", moves[m][2], ", will collide with a body")
 
-        print("Score for moving", moves[n][2],"is:", score[n])
+        elif nextMove in potentialCollisions(game_state):
+            print("Cannot move", moves[m][2], ", may collide with a snake")
+            score[m] *= chanceCollisionWeight
+        
+        print("Score for moving", moves[m][2], "is:", score[m])
 
     # Flood Fill
     # for possible moves above -100, check which place has more area
@@ -177,4 +229,4 @@ def move(game_state: typing.Dict) -> typing.Dict:
 if __name__ =="__main__":
     from server import run_server
 
-    run_server({"info": info,"start": start,"move": move,"end": end})
+    run_server({"info": info, "start": start, "move": move, "end": end})
